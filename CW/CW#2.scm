de3aec34c1ba9180eafdbadeee4cd092e2bbb54c
Kellen Yu <80976955+IlIll101@users.noreply.github.com>
2022-09-13 13:50:57 -0400

    adawdawd

---
+++ CW/CW#2.scm
@@ -0,0 +1,28 @@
+;3a
+(define d 2)
+(define u 7)
+(+ (* -5 d) (* -6 u) -3)
+;3b
+(define q -10)
+(expt q 2)
+;4b
+(define b 0)
+(expt (- (* 5 b) -6) 2)
+;5a
+(define p -3)
+(/ (+ (* 7 p) 5) -5)
+;5b
+(define q1 -4)
+(define g 1)
+(/ (- (* 6 q) g) 4)
+;6a
+(define q2 1)
+(define r 7)
+;7a
+(define d1 -5)
+(define s -2)
+(* d1 (- -7 s))
+;7b
+(define p1 -3)
+(define c -8)
+(- p1 c)
