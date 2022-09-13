13d775d02edbd7c979450762a5b5c2f1be33c8e5
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2022-09-12 22:06:05 -0400

    bruh

---
+++ HW/HW#1.scm
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HW#1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(/ (+ 5 4) (- 15 2))
+
+(/ (+ 7 (* 5 (sqrt 9))) (+ (expt 2 2) (/ 12 4)))
+
+(/ (+ (/ 4 8) 3) (- (/ 15 3) (/ 9 3)))
