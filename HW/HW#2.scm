3561ddb3d4d08b596088115cbf3292d2aba1c12b
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2022-09-13 20:09:58 -0400

    9/13

---
+++ HW/HW#2.scm
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HW#1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define u 11)
+(/ (- (* 3 u) 1) 8)
+
+(define v 5)
+(/ (* (+ (* 2 v) 5) 2) 6)
+
+(define w -4)
+(/ (* (- 8 w) 7) 4)
+
+(define x 5)
+(+ (* (- (* 7 x) 3) 4) (* (+ (* 3 x) 8) 2))
+
+(define y -6)
+(- (* (+ (* 2 y) 4) 8) (* (- (* 5 y) 10) 3))
+
+(define a 3)
+(define b 4)
+(define c -2)
+(/ (* (+ (* 2 a) (expt b 2)) 3) (* 4 c))
+
