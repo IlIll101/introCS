f4f199661f55ab816380596127bdb02f0f097107
Kellen Yu <thanoscarthanoscar420420@gmail.com>
2022-09-14 23:26:43 -0400

    newtons method

---
+++ NewtonsMethod.scm
@@ -0,0 +1,16 @@
+#lang racket
+;Finds the x intercept of the tangent line of the point (x, x^2-k)
+(define tngntln
+  (lambda (k x)
+    (/ (- (* 2 (expt x 2)) (- (expt x 2) k)) (* 2 x))
+    )
+  )
+
+;Newtons method to approximate the sqrt(k)
+(define (approx k)
+  (define x 10.0)
+  (for/list ([i 9])
+    (set! x (tngntln k x))
+    )
+  (display x)
+  )
