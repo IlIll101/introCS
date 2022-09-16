117427413a47c145a94b3a71046a6a675b084cb1
Kellen Yu <80976955+IlIll101@users.noreply.github.com>
2022-09-16 13:53:54 -0400

    amonger

---
+++ HW/HW#5.scm
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname HW#5) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (HarMean3 a b c)
+  (/ 3 (+ (/ 1 a) (/ 1 b) (/ 1 c)))
+  )
